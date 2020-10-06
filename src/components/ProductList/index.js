4dc69d0ad60f6260f5dd1acc82633d73b26c66b0
Melvin Gaye <melvind.gaye@gmail.com>
2020-10-05 20:49:28 -0400

    completed handlleUpvote

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -14,9 +14,18 @@ class ProductList extends React.Component{
     componentDidMount(){
         this.setState({products: products})
     }
-    
+
     handleUpVote(productId){
-        alert(productId)
+      const nextProducts = this.state.products.map((product)=>{
+          if(product.id === productId){
+              return Object.assign({}, product, {
+                  votes: product.votes + 1,
+              });
+          } else {
+              return product;
+          }
+      });
+      this.setState({products: nextProducts})
     }
 
     render(){
